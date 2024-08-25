dfdd8a3ee72784f0487c69537ba9a98a49ff26ab
beggarstudios <beggarstudios@gmail.com>
2024-08-25 20:07:32 +0200

    More tutorial stuff

---
+++ src/app.rs
@@ -0,0 +1,62 @@
+pub enum CurrentScreen {
+    Main,
+    Editing,
+    Exiting
+}
+
+pub enum CurrentlyEditing {
+    Key,
+    Value
+}
+
+pub struct App {
+    pub key_input: String,
+    pub value_input: String,
+    pub pairs: Hashmap<String, String>,
+    pub current_screen: CurrentScreen,
+    pub currently_editing: Option<CurrentlyEditing>
+}
+
+impl App {
+
+    /// Constructor function
+    pub fn new() -> App {
+        App {
+            key_input: String::new(),
+            value_input: String::new(),
+            pairs: Hashmap::new(),
+            current_screen: CurrentScreen::Main,
+            currently_editing: None,
+        }
+    }
+
+    /// Saving values
+    pub fn save_key_value(&mut self) {
+        self.pairs
+            .insert(self.key_input.clone(), self.value_input.clone());
+
+        self.key_input = String::new();
+        self.value_input = String::new();
+        self.currently_editing = None;
+    }
+
+    /// Toggle editing on, or between Key and Value fields
+    pub fn toggle_editing(&mut self) {
+        if let Some(edit_mode) = &self.currently_editing {
+            match edit_mode {
+                CurrentlyEditing::Key => self.currently_editing = Some(CurrentlyEditing::Value),
+                CurrentlyEditing::Value => self.currently_editing = Some(CurrentlyEditing::Key),
+            };
+        } else {
+            self.currently_editing = Some(CurrentlyEditing::Key);
+        }
+    }
+
+    /// Print JSON function
+    pub fn print_json(&self) -> serde_json::Result<()> {
+        let output = serde_json::to_string(&self.pairs)?;
+
+        println!("{}", output);
+        Ok(());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,196 +1,136 @@
-use std::io::{self, stdout};
-
-mod tui;
-
-#[derive(Debug, Default)]
-pub struct App {
-    counter: u8,
-    exit: bool,
-}
+use std::{error::Error, io};
 
 use ratatui::{
-    buffer::Buffer,
-    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    layout::{Alignment, Rect},
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{
-        block::{Position, Title},
-        Block, Paragraph, Widget,
+    backend::{Backend, CrosstermBackend},
+    crossterm::{
+        event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+        execute,
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     },
-    Frame,
+    Terminal,
 };
 
-use color_eyre::{
-    eyre::{bail, WrapErr},
-    Result,
+mod app;
+mod ui;
+use crate::{
+    app::{App, CurrentScreen, CurrentlyEditing},
+    ui::ui,
 };
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    let mut terminal= tui::init()?;
+fn main() -> Result<(), Box<dyn Error>> {
+    // Setup terminal
+    enable_raw_mode()?;
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
 
-    let app_result = App::default().run(&mut terminal);
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
 
-    if let Err(err) = tui::restore() {
-        eprintln!(
-             "failed to restore terminal. Run `reset` or restart your terminal to recover: {}",
-            err
-        )
-    }
+    // Create our app and run it
 
-    app_result
-}
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
 
-impl App {
-   /// runs the application's main loop until the user quits
-   pub fn run(&mut self, terminal: &mut tui::Tui) -> Result<()> {
-    while !self.exit {
-        terminal.draw(|frame| self.render_frame(frame))?;
-        self.handle_events().wrap_err("handle events failed")?;
-    }
-    Ok(())
-   }
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
-   fn render_frame(&self, frame: &mut Frame) {
-    frame.render_widget(self, frame.area()); 
-   }
-
-   fn handle_events(&mut self) -> Result<()> {
-        match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => self
-                .handle_key_event(key_event)
-                .wrap_err_with(|| format!("handling key event failed:\n{key_event:#?}")),
-            _ => Ok(()),
+    if let Ok(do_print) = res {
+        if do_print {
+            app.print_json()?;
         }
+    } else if let Err(err) = res {
+        println!("{err:?}");
     }
 
-   fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
-    match key_event.code {
-        KeyCode::Char('q') => self.exit(),
-        KeyCode::Left => self.decrement_counter()?,
-        KeyCode::Right => self.increment_counter()?,
-        _ => {}
-    }
     Ok(())
-   }
-
-   fn exit(&mut self) {
-    self.exit = true;
-   }
-
-   fn increment_counter(&mut self) -> Result<()> {
-    self.counter += 1;
-    if self.counter > 2 {
-        bail!("counter overflow");
-    }
-    Ok(())
-   }
-
-   fn decrement_counter(&mut self) -> Result<()> {
-    self.counter -= 1;
-    Ok(())
-   }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Counter App Tutorial ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        let block = Block::bordered()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .border_set(border::THICK);
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
+    loop {
+        terminal.draw(|f| ui (f, app))?;
 
-    let counter_text = Text::from(vec![Line::from(vec![
-        "Value: ".into(),
-        self.counter.to_string().yellow(),
-    ])]);
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // Skip events that are not KeyEventKind::Press
+                continue;
+            }
 
-    Paragraph::new(counter_text)
-        .centered()
-        .block(block)
-        .render(area, buf);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ratatui::style::Style;
-
-    #[test]
-    fn render() {
-        let app = App::default();
-        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        app.render(buf.area, &mut buf);
-
-        let mut expected = Buffer::with_lines(vec!{
-            "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
-            "┃                    Value: 0                    ┃",
-            "┃                                                ┃",
-            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        });
-        let title_style =Style::new().bold();
-        let counter_style = Style::new().yellow();
-        let key_style = Style::new().blue().bold();
-        expected.set_style(Rect::new(14, 0, 22, 1), title_style);
-        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-        assert_eq!(buf, expected);
-    }
-
-    #[test]
-    fn handle_key_event() -> io::Result<()> {
-        let mut app = App::default();
-
-        app.handle_key_event(KeyCode::Right.into()).unwrap();
-        assert_eq!(app.counter, 1);
-
-        app.handle_key_event(KeyCode::Left.into()).unwrap();
-        assert_eq!(app.counter, 0);
-
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Char('q').into()).unwrap();
-        assert!(app.exit);
-
-        Ok(())
-    }
-
-    #[test]
-    #[should_panic(expected = "attempt to subtract with overflow")]
-    fn handle_key_event_panic() {
-        let mut app = App::default();
-        let _ = app.handle_key_event(KeyCode::Left.into());
-    }
-
-    #[test]
-    fn handle_key_event_overflow() {
-        let mut app = App::default();
-        assert!(app.handle_key_event(KeyCode::Right.into()).is_ok());
-        assert!(app.handle_key_event(KeyCode::Right.into()).is_ok());
-        assert_eq!(
-            app.handle_key_event(KeyCode::Right.into())
-                .unwrap_err()
-                .to_string(),
-            "counter overflow"
-        );
+            match app.current_screen {
+                CurrentScreen::Main => match key.code {
+                    KeyCode::Char('e') => {
+                        app.current_screen = CurrentScreen::Editing;
+                        app.currently_editing = Some(CurrentlyEditing::Key);
+                    }
+                    KeyCode::Char('q') => {
+                        app.current_screen = CurrentScreen::Exiting;
+                    }
+                    _ => {}
+                },
+               CurrentScreen::Exiting => match key.code {
+                    KeyCode::Char('y') => {
+                        return Ok(true);
+                    }
+                    KeyCode::Char('n') | KeyCode::Char('q') => {
+                        return Ok(false);
+                    }
+                    _ => {}
+               },
+               CurrentScreen::Editing if key.kind == event::KeyEventKind::Press => {
+                match key.code {
+                    KeyCode::Enter => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.currently_editing = Some(CurrentlyEditing::Value)
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.save_key_value();
+                                    app.current_screen = CurrentScreen::Main;
+                                }
+                            }
+                        }
+                    }
+                    KeyCode::Backspace => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.key_input.pop();
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.value_input.pop();
+                                }
+                            }
+                        }
+                    }
+                    KeyCode::Esc => {
+                        app.current_screen = CurrentScreen::Main;
+                        app.currently_editing = None;
+                    }
+                    KeyCode::Tab => {
+                        app.toggle_editing();
+                    }
+                    KeyCode::Char(value) => {
+                        if let Some(editing) = &app.currently_editing {
+                            match editing {
+                                CurrentlyEditing::Key => {
+                                    app.key_input.push(value)
+                                }
+                                CurrentlyEditing::Value => {
+                                    app.value_input.push(value);
+                                }
+                            }
+                        }
+                    }
+                    _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
     }
 }
